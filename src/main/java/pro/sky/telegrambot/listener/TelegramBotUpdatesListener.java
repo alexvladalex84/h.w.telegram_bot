e544c88b8c6a787e5478a1a8f2ba16b3eaa5162d
Alexandrov <alex.44747.alex@gmail.com>
2023-11-01 09:17:15 +0300

    сообщение на команду старт

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @PostConstruct
     public void init() {
+
         telegramBot.setUpdatesListener(this);
     }
 
@@ -28,7 +31,13 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            Long chatId = update.message().chat().id();
+            if (update.message().text().equals("/start")) {
+                SendMessage message = new SendMessage(chatId,"Добро пожаловать");
+                SendResponse sendResponse = telegramBot.execute(message);
+            }
+
+
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
